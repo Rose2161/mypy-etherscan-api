2da3db0360d8d35b3aed755c4dea645de5109006
Cryptob3auty <76785638+Rose2161@users.noreply.github.com>
2021-09-08 21:52:21 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
 setuptools.setup(
-    name='py_etherscan_api',
+    name=mypy_etherscan_api',
     version='0.8.0',
     packages=['examples', 'examples.stats', 'examples.tokens',
               'examples.accounts', 'examples.blocks', 'examples.transactions',  'etherscan'],
-    url='https://github.com/corpetty/py-etherscan-api',
-    license='MIT',
-    author='coreypetty',
-    author_email='petty.btc@gmail.com',
+    url='https://github.com/Rose2161/mypy-etherscan-api',
+    license='The Unlicense',
+    author_changeS='Rose2161, 
+    author_email='aisharose2161@gmail.com',
     description='Python Bindings to Etherscan.io API',
     install_requires=[
         'requests>=2.20.0',
