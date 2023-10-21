bab794cfc9994c452b68f23211b8a9cbae5c38b9
Cryptob3auty <76785638+Rose2161@users.noreply.github.com>
2023-10-21 08:35:46 -0700

    Update test_transactions.py

--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -3,7 +3,7 @@ import warnings
 
 from etherscan.transactions import Transactions
 
-API_KEY = 'YourAPIkey'
+API_KEY = 'FCNR79749SDWAYAVAE2RBWHDZUMAKIXQUG'
 TX_1 = '0x15f8e5ea1079d9a0bb04a4c58ae5fe7654b5b2b4463375ff7ffb490aa0032f3a'
 TX_2 = '0x513c1ba0bebf66436b5fed86ab668452b7805593c05073eb2d51d3a52f480a76'
 ERROR_STRING = 'Bad jump destination'
