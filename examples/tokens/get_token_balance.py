5224cebff675937b45fa7608e93fcc7447929ef7
Cryptob3auty <76785638+Rose2161@users.noreply.github.com>
2025-07-27 06:07:37 -0700

    Update get_token_balance.py

--- examples/tokens/get_token_balance.py
+++ examples/tokens/get_token_balance.py
@@ -4,8 +4,8 @@ import json
 with open('../../api_key.json', mode='r') as key_file:
     key = json.loads(key_file.read())['key']
 
-address = '0xe04f27eb70e025b78871a2ad7eabe85e61212761'
-api = Tokens(contract_address='0x57d90b64a1a57749b0f932f1a3395792e12e7055',
+address = '0x4f19f6f747f43a3d9fcf8bb7d2214e798cd2ece8'
+api = Tokens(contract_address='0xdAC17F958D2ee523a2206206994597C13D831ec7',
              api_key=key)
 balance = api.get_token_balance(address=address)
 print(balance)
